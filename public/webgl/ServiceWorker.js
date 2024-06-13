2a39d0eb6995662e096cfe674a47ba3b6ffafd01
Zack <zkkzkk32312@msn.com>
2024-06-13 15:53:59 -0600

    Test webgl gzip build with .htaccess

---
+++ public/webgl/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "HardcoreDev-IOT_House-0.1.0";
+const contentToCache = [
+    "Build/20240613_Gzip.loader.js",
+    "Build/20240613_Gzip.framework.js.gz",
+    "Build/20240613_Gzip.data.gz",
+    "Build/20240613_Gzip.wasm.gz",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
